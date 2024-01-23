c3a9ebfd31e11b1472398312999cd606f8473014
GetPsyched6 <roshin006@gmail.com>
2024-01-23 21:54:56 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,10 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript with Node.js!');
+  res.status(418).send('Hello, TypeScript with Node.js!');
 });
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`Server running on http://localhost:${PORT}`);
 });
