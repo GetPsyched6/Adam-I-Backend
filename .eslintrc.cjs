76f052f5bb5261c3a7a42617b21644d69b30e936
GetPsyched6 <roshin006@gmail.com>
2024-01-23 21:45:44 +0530

    start backend

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: false,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+        printWidth: 100,
+        tabWidth: 2,
+        semi: true,
+        endOfLine: 'lf',
+        arrowParens: 'avoid',
+      },
+    ],
+  },
+  settings: {
+    // Node.js specific settings if needed
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+};
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get("/", (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript with Node.js!');
 });
 
 app.listen(PORT, () => {
-	console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
