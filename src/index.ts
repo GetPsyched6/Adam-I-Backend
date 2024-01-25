bf55a0a778068a0fed8e67df33619e20e71ec81f
GetPsyched6 <roshin006@gmail.com>
2024-01-25 18:15:57 +0530

    small eslint fixes

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
-app.use(userRoutes)
-app.use(companyRoutes)
+app.use(userRoutes);
+app.use(companyRoutes);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
